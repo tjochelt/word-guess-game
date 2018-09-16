adf376d15fe9c888b414c31d4079a01717d3f38b
Tom Ocheltree <tom.ocheltree@gmail.com>
2018-09-16 16:31:24 -0500

    files created

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//create html shell which includes title, instructions, and other sub sections
+//create list of possibile words to be drawn from
+//logic to add a number of underlines which corresponds to the number of letters in a word
+//display number of guesses on UI - if user guesses letter within word, display on applicable space, else subtract 1 from number of guesses and display the letter under letters already guessed
+//if number of guesses remaining = 0, display "Game over"
+//if word complete, display "You Win!" or something fancier if i have time
+
+var hawkIcons = ["kinnick", "long", "banks", "greene"]
+
