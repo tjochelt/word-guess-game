f94d307dd8202fdf218004f199263cc210d0ccdc
Tom Ocheltree <tom.ocheltree@gmail.com>
2018-09-19 23:01:48 -0500

    printing letters to screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,38 @@
 //create html shell which includes title, instructions, and other sub sections
 //create list of possibile words to be drawn from
-
-var hawkIcons = ["kinnick", "long", "banks", "greene", "duncan", "shaw", "tate", "sanders", "gallery"];
-
+var hawkIcons = [
+  "kinnick",
+  "long",
+  "banks",
+  "greene",
+  "duncan",
+  "shaw",
+  "tate",
+  "sanders",
+  "gallery"
+];
 
 // pick random word out of icons list
 var wordPick = hawkIcons[Math.floor(Math.random() * hawkIcons.length)];
 // console.log(wordPick)
 
-//logic to add a number of underlines which corresponds to the number of letters in a word
-
-
-// var underlineSpan = document.getElementById("spaces");
+//adds underscores for picked word
 var underlineWord = "";
-
-// asterisksSpan.textContent
-// asterisksSpan.innerHTML
-// asterisksSpan.innerText
-
 
 for (i = 0; i < wordPick.length; i++) {
   underlineWord += "_ ";
 }
-
 document.getElementById("spaces").innerHTML = underlineWord + "_ ";
+
+//prints the letter you type to the page
+document.onkeyup = function(event) {
+  var userInput = event.key;
+  document.getElementById("guessed").innerHTML = userInput;
+}
+// function showLetter() {
+//   document.getElementById("guessed").innerHTML = onkeyup.char;
+//  onclick(showLetter())
+// }
 
 
 //determine if guess is in chosenWord
@@ -43,7 +52,6 @@ var rightGuess = "";
 //   userText.textContent = event.key;
 // }}
 
-
 // var lettersGuessed
 
 // document.getElementById("demo").innerHTML = 5 + 6;
@@ -56,11 +64,8 @@ var rightGuess = "";
 // Let's start by grabbing a reference to the <span> below.
 
 // document.onkeyup = function(){myScript};
-  // userGuess.textContent = event.key;
-  // ("guessed").append(" " + event.key) 
-
-
-
+// userGuess.textContent = event.key;
+// ("guessed").append(" " + event.key)
 
 //if number of guesses remaining = 0, display "Game over"
 //if word complete, display "You Win!" or something fancier if i have time
