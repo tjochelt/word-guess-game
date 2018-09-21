087fb5ab5e46d248ebb547f192405cea9cedb5e9
Tom Ocheltree <tom.ocheltree@gmail.com>
2018-09-20 23:55:39 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,29 @@
-//create html shell which includes title, instructions, and other sub sections
-//create list of possibile words to be drawn from
-var hawkIcons = [
+
+//Pseudo code iteration:
+// create word repository using array
+
+//have computer pick random word out of array using math.random
+
+//based on the number of letters in the word, print that number of spaces to the dom using a for loop
+
+//when user types in a letter, game must validate if letter is in the word
+    //1. If: letter is in word, game should find the index of the correct letter in the string, and replace the space with the letter
+    //2. Else: ((display letter in list of "chosen letters") and (subtract one from guess total))
+
+//If guess count variable = 0
+    //alert game over notification
+    //Add one to loss total
+    //Reset guess total variable
+    //Clear guessed letters list
+
+//If number of underscores displayed on screen = 0, user wins game
+    //Add one to win total
+    //Reset guess total variable
+    //Clear guessed letters list
+
+//Tom's actual attempts:
+//list of possible Iowa Hawkeye greats to choose from
+    var hawkIcons = [
   "kinnick",
   "long",
   "banks",
@@ -20,9 +43,8 @@ console.log(wordPick);
 var underlineWord = "";
 for (i = 0; i < wordPick.length; i++) {
   underlineWord += "_ ";
-
-document.getElementById("spaces").innerHTML = underlineWord + "_ ";
-console.log(underlineWord);
+// document.getElementById("spaces").innerHTML = underlineWord + "_ ";
+// console.log(underlineWord);
 } 
 
 //splits the chosen word apart
@@ -41,11 +63,11 @@ document.onkeyup = function(event) {
   for (var i = 0;i < indLetters.length; i++) {
     if (userInput === indLetters[i].toUpperCase()) {
       indLetters[i] = wordPick[i];
-    } else (userInput != indLetters[i].toUpperCase()) {
+    } else (userInput != indLetters[i].toUpperCase()) 
       document.getElementById("guessed").innerHTML = userInput;  
     }
   }
-}
+
   // if (wordPick.indexOf(userInput) < 0) {
   //   document.getElementById("guessed").innerHTML = userInput;
   // } else {
